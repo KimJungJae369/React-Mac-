6a784d0957d1030f72317cd466d4ca1f3ae08c3e
KimJungJae369 <ktk662002@naver.com>
2025-10-05 15:58:16 +0900

    Add files via upload

---
+++ Component7/User.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function User(props) {
+    return (
+        <>
+            <p>name : {props.name}</p>
+            <p>name : {props.age}</p>
+        </>
+    )
+}
